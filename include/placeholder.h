13e61e6dbad6c1fade1a1c313bdd52915a41ba18
chadd williams <will4614@pacificu.edu>
2022-05-29 21:58:31 -0700

    add examples

--- include/placeholder.h
+++
@@ -1,2 +0,0 @@
-// this file is a placeholder to keep make happy
-// if you add your own .h file you may remove this file.
--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,61 @@
 #include <vector> 
 #include <string>
 #include "ExampleData.h"
+#include <algorithm>
 
 int main()
 {
     std::vector<int> cVecInts = {1,2,3,4,5};
+    std::vector<int> cVecNewInts;
     std::vector<ExampleData> cVecData = {ExampleData(1,"a"), ExampleData(2,"b"), ExampleData(3,"c")};
 
 
-    //for int index
+    std::cout << "Old Style for with int index:" << std::endl;
+    // old style for with int index
+    for ( int i = 0; i < cVecData.size() ; ++i )
+    {
+        std::cout << cVecInts[i] << std::endl;
+    }
 
     // for (auto it = vec.begin() ; it != vec.end(); ++it) {it->work();} 
-    
+    std::cout << "For with iterator" << std::endl;
+    for( auto iter = cVecData.begin() ; iter != cVecData.end(); ++iter)
+    {
+        iter->outputMessage(std::cout); // iter is a pointer
+        std::cout << std::endl;
+    }
+
     // range base
-    // for(auto it : vec) { it->work(); }
+    // for(auto it : vec) { it.work(); }
+    std::cout << "Range Based For" << std::endl;
+    for(auto &iter : cVecInts)  // auto &iter -- iter is a reference (mutable)
+    {
+        std::cout << iter << std::endl; // iter is an object
+        iter *= 3;
+    }
 
     // std::for_each(vec.begin, vec.end(), [](auto &rData) { rData->work();});
+    std::cout << "For each" << std::endl;
+    std::for_each(cVecInts.begin(), cVecInts.end(), [](auto &rData) { std::cout << rData << std::endl;});
 
     // C++20
-    // std::for_each(vec, [](auto &rPiece) {std::cout << rPiece;}, [](const auto& rData) { return rData.doubleData(); } ;
-
+    // std::ranges::for_each(vec, [](const auto &rPiece) {std::cout << rPiece;}, [](const auto& rData) { return rData.doubleData(); } ;
+    std::cout << "For each over range" << std::endl;
+    std::ranges::for_each(cVecData, [](const auto &rData) {std::cout << rData << std::endl;},
+         [](const auto& rData) { return rData.doubleData(); } );
 
     // std::transform
-    // std::transform(vec.begin(), vec.end(), vec.begin(), [](auto &rData) { return rData * 2} );
-    
+    // std::transform(vec.begin(), vec.end(), vec.begin(), [](auto &rData) { return rData * 2;} );
+    std::cout << "Transform a vector" << std::endl;
+    std::transform(cVecInts.begin(), cVecInts.end(), cVecInts.begin(), [](auto &rData) { return rData * 2;} );
+    std::for_each(cVecInts.begin(), cVecInts.end(), [](auto &rData) { std::cout << rData << std::endl;});
+
     // std::transform(vec.begin(), vec.end(), vecNew.begin(), [](auto &rData) { return rData * 2} );
+    std::cout << "Transform a vector, copy to new vector" << std::endl;
+    std::transform(cVecInts.begin(), cVecInts.end(), std::back_inserter(cVecNewInts), [](auto &rData) { return rData * 2;} );
+    std::for_each(cVecInts.begin(), cVecInts.end(), [](auto &rData) { std::cout << rData << std::endl;});
+    std::cout << "cVecNewInts" << std::endl;
+    std::for_each(cVecNewInts.begin(), cVecNewInts.end(), [](auto &rData) { std::cout << rData << std::endl;});
     
     return EXIT_SUCCESS;
 }
